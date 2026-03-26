78a67dbcac29d7d28da2f04e75b8804a441c3aa7
Louisa Tambunting <louisatambunting@gmail.com>
2026-03-26 16:33:13 -0700

    Add: refactored genotype field in Cat to separate genotype class, created phenotype class, tested and implemented Genotype

--- src/main/java/model/Phenotype.java
+++ src/main/java/model/Phenotype.java
@@ -1,12 +1,17 @@
 package model;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Phenotype {
-    private final Map<Trait, String> expressedVariants; //the expressed variant
+    private final Map<Trait, String> expressedVariants; //the expressed variant for each trait
     
     public Phenotype() {
         expressedVariants = new HashMap<>();
+    }
+
+    public Map<Trait, String> getExpressedVariants() {
+        return Collections.unmodifiableMap(expressedVariants);
     }
 }
