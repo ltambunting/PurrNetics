9b00415787487c630eba3c2e77d92be73dc6446f
Louisa Tambunting <louisatambunting@gmail.com>
2026-04-08 10:54:49 -0700

    Add: created tests for Phenotype class

--- src/main/java/model/Phenotype.java
+++ src/main/java/model/Phenotype.java
@@ -1,14 +1,13 @@
 package model;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Phenotype {
     private final Map<Trait, String> expressedVariants; //the expressed variant for each trait
     
-    public Phenotype() {
-        expressedVariants = new HashMap<>();
+    public Phenotype(Map<Trait, String> traitMap) {
+        expressedVariants = traitMap;
     }
 
     public Map<Trait, String> getExpressedVariants() {
