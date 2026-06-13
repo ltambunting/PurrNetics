65567d761f835ad8c405f003b3c673861d2692c5
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-12 22:26:25 -0700

    Add: completed implementation of XLinkedInheritance class

--- src/main/java/model/XLinkedInheritance.java
+++ src/main/java/model/XLinkedInheritance.java
@@ -3,18 +3,28 @@ package model;
 import java.util.Random;
 
 /**
- * Models inheritance of X-linked loci
- * 
+ * Models inheritance of X-linked loci.
+ *
  * Note: male offspring are represented using a special Y placeholder allele
- * to preserve compatability with the AllelePair-based genotype model. 
- * In other words, it indicates the absence of a corresponding X-linked allele on 
- * the Y chromosome in XY cats.
+ * to preserve compatibility with the AllelePair-based genotype model.
+ * The placeholder does not represent a true allele at this locus; rather,
+ * it indicates the absence of a corresponding X-linked allele on the Y chromosome.
  */
 public class XLinkedInheritance implements InheritanceRule {
 
     @Override
     public AllelePair inherit(AllelePair maternal, AllelePair paternal, Sex offspringSex, Random random) {
-        return null; //stub
+        Gene.Allele maternalInheritedAllele = null;
+        Gene.Allele paternalInheritedAllele = null;
+        if (offspringSex == Sex.FEMALE) {
+            maternalInheritedAllele = maternal.getRandomAllele(random);
+            paternalInheritedAllele = paternal.getMaternalAllele();
+        } else if (offspringSex == Sex.MALE) {
+            maternalInheritedAllele = maternal.getRandomAllele(random);
+            paternalInheritedAllele = paternal.getPaternalAllele();
+
+        }
+        return new AllelePair(maternalInheritedAllele, paternalInheritedAllele);
     }
 
 }
