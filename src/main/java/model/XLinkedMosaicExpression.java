54a42d1e29cf6014df540086268b716bc405a76f
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-14 13:14:40 -0700

    Add: XLinkedMosaicExpression class and test class set up

---
+++ src/main/java/model/XLinkedMosaicExpression.java
@@ -0,0 +1,10 @@
+package model;
+
+public class XLinkedMosaicExpression implements ExpressionRule {
+
+    @Override
+    public String resolvePhenotype(AllelePair allelePair) {
+        return "";
+    }
+    
+}
---
+++ src/test/java/model/XLinkedMosaicExpressionTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Random;
+
+public class XLinkedMosaicExpressionTest {
+    private static final int ITERATIONS = 10000;
+    private static final String ORANGE_VARIANT = "Orange";
+    private static final String NON_ORANGE_VARIANT = "Non-Orange";
+    private Random random;
+    private Gene gene;
+    private Trait trait;
+    private InheritanceRule XInheritanceRule;
+    private XLinkedMosaicExpression xLinkedMosaicExpression;
+    private Gene.Allele orangeAllele;
+    private Gene.Allele nonOrangeAllele;
+    private Gene.Allele YChromosomeRepresentation;
+
+    
+}
