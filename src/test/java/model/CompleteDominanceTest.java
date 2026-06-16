bb75d1c387dbae2db6b38a374d1d93fe0e8dc252
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-15 18:08:56 -0700

    Adj: removed redundant tests and modified invariant testing for CompleteDominanceTest

--- src/test/java/model/CompleteDominanceTest.java
+++ src/test/java/model/CompleteDominanceTest.java
@@ -1,11 +1,8 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -57,173 +54,18 @@ public class CompleteDominanceTest {
     }
 
     @Test
-    public void monohybridCrossDistributionTest() {
-        // expect 3:1 ratio of dominant to recessive trait
-        AllelePair hybrid1 = new AllelePair(recessiveAllele, dominantAllele);
-        AllelePair hybrid2 = new AllelePair(dominantAllele, recessiveAllele);
+    public void AaAndaAAlwaysProduceSamePhenotypeTest() {
+        // should both be dominant phenotype
+        AllelePair allelePairAa = new AllelePair(dominantAllele, recessiveAllele);
+        AllelePair allelePairaA = new AllelePair(recessiveAllele, dominantAllele);
 
-        List<AllelePair> offspringAllelePairs = makeManyOffspringAllelePairsHelper(hybrid1, hybrid2);
-
-        int dominantCount = 0;
-        int recessiveCount = 0;
-
-        for (AllelePair offspring : offspringAllelePairs) {
-            String expressedSingularPhenotype = completeDominance.resolvePhenotype(offspring);
-            if (expressedSingularPhenotype.equals(DOMINANT_TRAIT)) {
-                dominantCount++;
-            } else if (expressedSingularPhenotype.equals(RECESSIVE_TRAIT)) {
-                recessiveCount++;
-            }
-        }
-
-        double fractionDominantTrait = (double) dominantCount / ITERATIONS;
-        double fractionRecessiveTrait = (double) recessiveCount / ITERATIONS;
-
-        assertTrue(fractionDominantTrait > 0.7 && fractionDominantTrait < 0.8);
-        assertTrue(fractionRecessiveTrait > 0.2 && fractionRecessiveTrait < 0.3);
-    }
-
-    @Test
-    public void homozygousDominantHomozygousRecessiveCrossDistributionTest() {
-        // expect 1:0 ratio of dominant to recessive trait
-        AllelePair homozygousDominant = new AllelePair(dominantAllele, dominantAllele);
-        AllelePair homozygousRecessive = new AllelePair(recessiveAllele, recessiveAllele);
-
-        List<AllelePair> offspringAllelePairs = makeManyOffspringAllelePairsHelper(homozygousDominant, homozygousRecessive);
-
-        int dominantCount = 0;
-        int recessiveCount = 0;
-
-        for (AllelePair offspring : offspringAllelePairs) {
-            String expressedSingularPhenotype = completeDominance.resolvePhenotype(offspring);
-            if (expressedSingularPhenotype.equals(DOMINANT_TRAIT)) {
-                dominantCount++;
-            } else if (expressedSingularPhenotype.equals(RECESSIVE_TRAIT)) {
-                recessiveCount++;
-            }
-        }
-
-        double fractionDominantTrait = (double) dominantCount / ITERATIONS;
-        double fractionRecessiveTrait = (double) recessiveCount / ITERATIONS;
-
-        assertTrue(fractionDominantTrait == 1.0);
-        assertTrue(fractionRecessiveTrait == 0.0);
-    }
-
-    @Test
-    public void bothHomozygousRecessiveCrossDistributionTest() {
-        // expect 1:0 ratio of recessive to dominant trait
-        AllelePair homozygousRecessive1 = new AllelePair(recessiveAllele, recessiveAllele);
-        AllelePair homozygousRecessive2 = new AllelePair(recessiveAllele, recessiveAllele);
-
-        List<AllelePair> offspringAllelePairs = makeManyOffspringAllelePairsHelper(homozygousRecessive1, homozygousRecessive2);
-
-        int dominantCount = 0;
-        int recessiveCount = 0;
-
-        for (AllelePair offspring : offspringAllelePairs) {
-            String expressedSingularPhenotype = completeDominance.resolvePhenotype(offspring);
-            if (expressedSingularPhenotype.equals(DOMINANT_TRAIT)) {
-                dominantCount++;
-            } else if (expressedSingularPhenotype.equals(RECESSIVE_TRAIT)) {
-                recessiveCount++;
-            }
-        }
-
-        double fractionDominantTrait = (double) dominantCount / ITERATIONS;
-        double fractionRecessiveTrait = (double) recessiveCount / ITERATIONS;
-
-        assertTrue(fractionDominantTrait == 0.0);
-        assertTrue(fractionRecessiveTrait == 1.0);
-    }
-
-    @Test
-    public void bothHomozygousDominantCrossDistributionTest() {
-        // expect 1:0 ratio of dominant to recessive trait
-        AllelePair homozygousDominant1 = new AllelePair(dominantAllele, dominantAllele);
-        AllelePair homozygousDominant2 = new AllelePair(dominantAllele, dominantAllele);
-
-        List<AllelePair> offspringAllelePairs = makeManyOffspringAllelePairsHelper(homozygousDominant1, homozygousDominant2);
-
-        int dominantCount = 0;
-        int recessiveCount = 0;
-
-        for (AllelePair offspring : offspringAllelePairs) {
-            String expressedSingularPhenotype = completeDominance.resolvePhenotype(offspring);
-            if (expressedSingularPhenotype.equals(DOMINANT_TRAIT)) {
-                dominantCount++;
-            } else if (expressedSingularPhenotype.equals(RECESSIVE_TRAIT)) {
-                recessiveCount++;
-            }
-        }
-
-        double fractionDominantTrait = (double) dominantCount / ITERATIONS;
-        double fractionRecessiveTrait = (double) recessiveCount / ITERATIONS;
-
-        assertTrue(fractionDominantTrait == 1.0);
-        assertTrue(fractionRecessiveTrait == 0.0);
-    }
-
-    @Test
-    public void homozygousDominantMonohybridCrossDistributionTest() {
-        // expect 1:0 ratio of dominant to recessive trait
-        AllelePair homozygousDominant = new AllelePair(dominantAllele, dominantAllele);
-        AllelePair hybrid = new AllelePair(dominantAllele, recessiveAllele);
-
-        List<AllelePair> offspringAllelePairs = makeManyOffspringAllelePairsHelper(homozygousDominant, hybrid);
-
-        int dominantCount = 0;
-        int recessiveCount = 0;
-
-        for (AllelePair offspring : offspringAllelePairs) {
-            String expressedSingularPhenotype = completeDominance.resolvePhenotype(offspring);
-            if (expressedSingularPhenotype.equals(DOMINANT_TRAIT)) {
-                dominantCount++;
-            } else if (expressedSingularPhenotype.equals(RECESSIVE_TRAIT)) {
-                recessiveCount++;
-            }
-        }
-
-        double fractionDominantTrait = (double) dominantCount / ITERATIONS;
-        double fractionRecessiveTrait = (double) recessiveCount / ITERATIONS;
-
-        assertTrue(fractionDominantTrait == 1.0);
-        assertTrue(fractionRecessiveTrait == 0.0);
-    }
-
-    @Test
-    public void homozygousRecessiveMonohybridCrossDistributionTest() {
-        // expect 1:1 ratio of dominant to recessive trait
-        AllelePair homozygousRecessive = new AllelePair(recessiveAllele, recessiveAllele);
-        AllelePair hybrid = new AllelePair(dominantAllele, recessiveAllele);
-
-        List<AllelePair> offspringAllelePairs = makeManyOffspringAllelePairsHelper(homozygousRecessive, hybrid);
-
-        int dominantCount = 0;
-        int recessiveCount = 0;
-
-        for (AllelePair offspring : offspringAllelePairs) {
-            String expressedSingularPhenotype = completeDominance.resolvePhenotype(offspring);
-            if (expressedSingularPhenotype.equals(DOMINANT_TRAIT)) {
-                dominantCount++;
-            } else if (expressedSingularPhenotype.equals(RECESSIVE_TRAIT)) {
-                recessiveCount++;
-            }
-        }
-
-        double fractionDominantTrait = (double) dominantCount / ITERATIONS;
-        double fractionRecessiveTrait = (double) recessiveCount / ITERATIONS;
-
-        assertTrue((fractionDominantTrait > 0.45) && (fractionDominantTrait < 0.55));
-        assertTrue((fractionRecessiveTrait > 0.45) && (fractionRecessiveTrait < 0.55));
-    }
-
-    public List<AllelePair> makeManyOffspringAllelePairsHelper(AllelePair allelepair1, AllelePair allelepair2) {
-        List<AllelePair> offspringAllelePairs = new ArrayList<>();
         for (int i = 0; i < ITERATIONS; i++) {
-            AllelePair offspring = autosomalInheritance.inherit(allelepair1, allelepair2, Sex.FEMALE, random);
-            offspringAllelePairs.add(offspring);
+            String phenotypeAa = completeDominance.resolvePhenotype(allelePairAa);
+            String phenotypeaA = completeDominance.resolvePhenotype(allelePairaA);
+            assertEquals(DOMINANT_TRAIT, phenotypeAa);
+            assertEquals(DOMINANT_TRAIT, phenotypeaA);
         }
-        return offspringAllelePairs;
     }
+    
 }
+
