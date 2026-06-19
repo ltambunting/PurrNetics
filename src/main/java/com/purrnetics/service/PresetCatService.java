ce7dc715b8a2d8ab29a6e41830adf224c5b23c3d
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-19 10:38:33 -0700

    Correction: pushed PresetCatService

--- src/main/java/com/purrnetics/service/PresetCatService.java
+++ src/main/java/com/purrnetics/service/PresetCatService.java
@@ -1,5 +1,25 @@
 package com.purrnetics.service;
 
-public class PresetCatService {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.purrnetics.factory.PresetCatFactory;
+import com.purrnetics.model.Cat;
+
+public class PresetCatService {
+    private PresetCatFactory presetCatFactory;
+
+    public PresetCatService(PresetCatFactory presetCatFactory) {
+        this.presetCatFactory = presetCatFactory;
+    }
+
+    public List<Cat> getPresetCatList() {
+        List<Cat> presetCatList = new ArrayList<>();
+        presetCatList.add(presetCatFactory.createFemaleShortHairAgoutiTortieCat());
+        presetCatList.add(presetCatFactory.createFemaleShortHairAgoutiOrangeCat());
+        presetCatList.add(presetCatFactory.createFemaleShortHairAgoutiNonOrangeCat());
+        presetCatList.add(presetCatFactory.createMaleShortHairAgoutiOrangeCat());
+        presetCatList.add(presetCatFactory.createMaleShortHairAgoutiNonOrangeCat());
+        return presetCatList;
+    }
 }
