19c640dc24d80b932d38ca8e5645c5c6ce9f5778
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-25 09:30:55 -0700

    Add: tested and implemented getPossibleGametes and getInheritanceDistribution function for XLinkedInheritance

--- src/main/java/com/purrnetics/model/XLinkedInheritance.java
+++ src/main/java/com/purrnetics/model/XLinkedInheritance.java
@@ -1,5 +1,7 @@
 package com.purrnetics.model;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -14,6 +16,7 @@ import java.util.Random;
  */
 public class XLinkedInheritance implements InheritanceRule {
     private static final Gene.Allele NO_X_ALLELE = null;
+
     @Override
     public AllelePair inherit(Gene gene, AllelePair maternal, AllelePair paternal, Sex offspringSex, Random random) {
         Gene.Allele maternalInheritedAllele = null;
@@ -28,5 +31,39 @@ public class XLinkedInheritance implements InheritanceRule {
         }
         return new AllelePair(gene, maternalInheritedAllele, paternalInheritedAllele);
     }
+
+    @Override
+    public Map<Gene.Allele, Double> getPossibleGametes(AllelePair parentAllelePair) {
+        Map<Gene.Allele, Double> possibleGameteDistributionMap = new HashMap<>();
+        Gene.Allele maternalAllele = parentAllelePair.getMaternalAllele();
+        Gene.Allele paternalAllele = parentAllelePair.getPaternalAllele();
+        if (maternalAllele == paternalAllele) {
+            possibleGameteDistributionMap.put(maternalAllele, 1.0);
+        } else {
+            possibleGameteDistributionMap.put(maternalAllele, 0.50);
+            possibleGameteDistributionMap.put(paternalAllele, 0.50);
+        }
+        return possibleGameteDistributionMap;
+    }
+
+    @Override
+    public Map<AllelePair, Double> getInheritanceDistribution(Gene gene, AllelePair maternalAllelePair, AllelePair paternalAllelePair) {
+        Map<Gene.Allele, Double> possibleMaternalGameteDistributionMap = getPossibleGametes(maternalAllelePair);
+        Map<Gene.Allele, Double> possiblePaternalGameteDistributionMap = getPossibleGametes(paternalAllelePair);
+        Map<AllelePair, Double> inheritanceDistributionMap = new HashMap<>();
+        for (Gene.Allele maternalAllele : possibleMaternalGameteDistributionMap.keySet()) {
+            for (Gene.Allele paternalAllele : possiblePaternalGameteDistributionMap.keySet()) {
+                AllelePair allelePair = new AllelePair(gene, maternalAllele, paternalAllele);
+                if (!inheritanceDistributionMap.containsKey(allelePair)) {
+                    Double probability = possibleMaternalGameteDistributionMap.get(maternalAllele) * 
+                        possiblePaternalGameteDistributionMap.get(paternalAllele);
+                    inheritanceDistributionMap.put(allelePair, probability);
+                }
+            }
+        }
+        return inheritanceDistributionMap;
+    }
+
+    
 
 }
--- src/test/java/com/purrnetics/model/XLinkedInheritanceTest.java
+++ src/test/java/com/purrnetics/model/XLinkedInheritanceTest.java
@@ -1,14 +1,17 @@
 package com.purrnetics.model;
 
+import java.util.Map;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class XLinkedInheritanceTest {
+    private static final Gene.Allele NO_X_ALLELE = null;
     private static final int ITERATIONS = 10000;
     private static final String ORANGE_TRAIT = "Orange Fur";
     private static final String NON_ORANGE_TRAIT = "Non-Orange Fur";
@@ -22,8 +25,11 @@ public class XLinkedInheritanceTest {
     private ExpressionRule completeDominance;
     private Gene.Allele orangeAllele;
     private Gene.Allele nonOrangeAllele;
-    private AllelePair maternalAllelePair;
-    private AllelePair paternalAllelePair;
+    private AllelePair maternalOrangePaternalNonOrangeFemaleAllelePair;
+    private AllelePair maternalNonOrangePaternalOrangeFemaleAllelePair;
+    private AllelePair orangeMaleAllelePair;
+    private AllelePair nonOrangeMaleAllelePair;
+    private AllelePair homozygousOrangeFemaleAllelePair;
 
     @BeforeEach
     public void setup() {
@@ -34,32 +40,35 @@ public class XLinkedInheritanceTest {
         gene = new Gene("O", trait, xLinkedInheritance, completeDominance);
         orangeAllele = gene.addAllele("O", 1, ORANGE_TRAIT);
         nonOrangeAllele = gene.addAllele("o", 1, NON_ORANGE_TRAIT);
-        maternalAllelePair = new AllelePair(gene, orangeAllele, nonOrangeAllele);
+        maternalOrangePaternalNonOrangeFemaleAllelePair = new AllelePair(gene, orangeAllele, nonOrangeAllele);
+        maternalNonOrangePaternalOrangeFemaleAllelePair = new AllelePair(gene, nonOrangeAllele, orangeAllele);
+        homozygousOrangeFemaleAllelePair = new AllelePair(gene, orangeAllele, orangeAllele);
         // paternal X-linked chromosome: second allele is absent (hemizygous Y-side)
-        paternalAllelePair = new AllelePair(gene, orangeAllele, null);
+        orangeMaleAllelePair = new AllelePair(gene, orangeAllele, NO_X_ALLELE);
+        nonOrangeMaleAllelePair = new AllelePair(gene, nonOrangeAllele, NO_X_ALLELE);
     }
 
     // verify daughters inherit a maternal X and father's X chromosome -> invariant
     @Test
     public void femaleOffspringInheritsMaternalXPaternalX() {
-        AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalAllelePair, paternalAllelePair, Sex.FEMALE, random);
+        AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalOrangePaternalNonOrangeFemaleAllelePair, orangeMaleAllelePair, Sex.FEMALE, random);
 
         Gene.Allele maternalInheritedAllele = offspringAllelePair.getMaternalAllele();
         Gene.Allele paternalInheritedAllele = offspringAllelePair.getPaternalAllele();
 
-        assertTrue(maternalInheritedAllele == maternalAllelePair.getMaternalAllele() || maternalInheritedAllele == maternalAllelePair.getPaternalAllele());
-        assertEquals(paternalInheritedAllele, paternalAllelePair.getMaternalAllele());
+        assertTrue(maternalInheritedAllele == maternalOrangePaternalNonOrangeFemaleAllelePair.getMaternalAllele() || maternalInheritedAllele == maternalOrangePaternalNonOrangeFemaleAllelePair.getPaternalAllele());
+        assertEquals(paternalInheritedAllele, orangeMaleAllelePair.getMaternalAllele());
     }
 
     // verify sons inherit a maternal X and father's Y chromosme -> invariant
     @Test
     public void maleOffspringInheritsMaternalXPaternalY() {
-        AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalAllelePair, paternalAllelePair, Sex.MALE, random);
+        AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalOrangePaternalNonOrangeFemaleAllelePair, orangeMaleAllelePair, Sex.MALE, random);
 
         Gene.Allele maternalInheritedAllele = offspringAllelePair.getMaternalAllele();
         Gene.Allele paternalInheritedAllele = offspringAllelePair.getPaternalAllele();
 
-        assertTrue(maternalInheritedAllele == maternalAllelePair.getMaternalAllele() || maternalInheritedAllele == maternalAllelePair.getPaternalAllele());
+        assertTrue(maternalInheritedAllele == maternalOrangePaternalNonOrangeFemaleAllelePair.getMaternalAllele() || maternalInheritedAllele == maternalOrangePaternalNonOrangeFemaleAllelePair.getPaternalAllele());
         assertNull(paternalInheritedAllele);
     }
 
@@ -68,7 +77,7 @@ public class XLinkedInheritanceTest {
     public void xLinkedInheritanceMaternalAlleleDistributionOfFemaleOffspring() {
         int numMaternalOrangeAlleles = 0, numMaternalNonOrangeAlleles = 0;
         for (int i = 0; i < ITERATIONS; i++) {
-            AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalAllelePair, paternalAllelePair, Sex.FEMALE, random);
+            AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalOrangePaternalNonOrangeFemaleAllelePair, orangeMaleAllelePair, Sex.FEMALE, random);
             Gene.Allele maternalInheritedAllele = offspringAllelePair.getMaternalAllele();
 
             if (maternalInheritedAllele == orangeAllele) {
@@ -89,7 +98,7 @@ public class XLinkedInheritanceTest {
     public void xLinkedInheritanceMaternalAlleleDistributionOfMaleOffspring() {
         int numMaternalOrangeAlleles = 0, numMaternalNonOrangeAlleles = 0;
         for (int i = 0; i < ITERATIONS; i++) {
-            AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalAllelePair, paternalAllelePair, Sex.MALE, random);
+            AllelePair offspringAllelePair = xLinkedInheritance.inherit(gene, maternalOrangePaternalNonOrangeFemaleAllelePair, orangeMaleAllelePair, Sex.MALE, random);
             Gene.Allele maternalInheritedAllele = offspringAllelePair.getMaternalAllele();
 
             if (maternalInheritedAllele == orangeAllele) {
@@ -103,5 +112,76 @@ public class XLinkedInheritanceTest {
 
         assertTrue(maternalOrangeAllelesProportion >= LOWER_BOUND_ALLELE_DISTRIBUTION && maternalOrangeAllelesProportion <= UPPER_BOUND_ALLELE_DISTRIBUTION);
         assertTrue(maternalNonOrangeAllelesProportion >= LOWER_BOUND_ALLELE_DISTRIBUTION && maternalNonOrangeAllelesProportion <= UPPER_BOUND_ALLELE_DISTRIBUTION);
+    }
+
+    // test gametes for heterozygous female
+    @Test
+    public void getPossibleGametesHeterozygousFemaleTest() {
+        Map<Gene.Allele, Double> possibleAllelesMap = xLinkedInheritance.getPossibleGametes(maternalOrangePaternalNonOrangeFemaleAllelePair);
+        assertTrue(possibleAllelesMap.containsKey(orangeAllele));
+        assertTrue(possibleAllelesMap.containsKey(nonOrangeAllele));
+        assertEquals(0.5, possibleAllelesMap.get(orangeAllele));
+        assertEquals(0.5, possibleAllelesMap.get(nonOrangeAllele));
+    }
+    // test gametes for homozygous female
+    @Test
+    public void getPossibleGametesHomozygousFemaleTest() {
+        AllelePair homozygousAllelePair = new AllelePair(gene, orangeAllele, orangeAllele);
+        Map<Gene.Allele, Double> possibleAllelesMap = xLinkedInheritance.getPossibleGametes(homozygousAllelePair);
+        assertTrue(possibleAllelesMap.containsKey(orangeAllele));
+        assertFalse(possibleAllelesMap.containsKey(nonOrangeAllele));
+        assertEquals(1.0, possibleAllelesMap.get(orangeAllele));
+        assertTrue(possibleAllelesMap.size() == 1);
+    }
+
+    // test gametes for male
+    @Test
+    public void getPossibleGametesMaleTest() {
+        Map<Gene.Allele, Double> possibleAllelesMap = xLinkedInheritance.getPossibleGametes(orangeMaleAllelePair);
+        assertTrue(possibleAllelesMap.containsKey(NO_X_ALLELE));
+        assertTrue(possibleAllelesMap.containsKey(orangeAllele));
+        assertFalse(possibleAllelesMap.containsKey(nonOrangeAllele));
+        assertEquals(0.5, possibleAllelesMap.get(NO_X_ALLELE));
+        assertEquals(0.5, possibleAllelesMap.get(orangeAllele));
+    }
+
+    // test heterozygous female and affected male
+    @Test
+    public void getInheritanceDistributionHeterozygousFemaleAffectedMaleTest() {
+        Map<AllelePair, Double> inheritanceDistributionMap = xLinkedInheritance.getInheritanceDistribution(gene, maternalOrangePaternalNonOrangeFemaleAllelePair, orangeMaleAllelePair);
+        assertTrue(inheritanceDistributionMap.size() == 4);
+        assertFalse(inheritanceDistributionMap.containsKey(maternalOrangePaternalNonOrangeFemaleAllelePair));
+        assertTrue(inheritanceDistributionMap.containsKey(maternalNonOrangePaternalOrangeFemaleAllelePair));
+        assertTrue(inheritanceDistributionMap.containsKey(homozygousOrangeFemaleAllelePair));
+        assertTrue(inheritanceDistributionMap.containsKey(nonOrangeMaleAllelePair));
+        assertTrue(inheritanceDistributionMap.containsKey(orangeMaleAllelePair));
+        for (AllelePair allelePair : inheritanceDistributionMap.keySet()) {
+            assertEquals(0.25, inheritanceDistributionMap.get(allelePair));
+        }
+    }
+
+    // test homozygous female and male with opposite copy
+    @Test
+    public void getInheritanceDistributionHomozygousAffectedFemaleNonAffectedMaleTest() {
+        Map<AllelePair, Double> inheritanceDistributionMap = xLinkedInheritance.getInheritanceDistribution(gene, homozygousOrangeFemaleAllelePair, nonOrangeMaleAllelePair);
+        assertTrue(inheritanceDistributionMap.size() == 2);
+        assertTrue(inheritanceDistributionMap.containsKey(maternalOrangePaternalNonOrangeFemaleAllelePair));
+        assertTrue(inheritanceDistributionMap.containsKey(orangeMaleAllelePair));
+        assertFalse(inheritanceDistributionMap.containsKey(nonOrangeMaleAllelePair));
+        assertFalse(inheritanceDistributionMap.containsKey(homozygousOrangeFemaleAllelePair));
+        assertEquals(0.5, inheritanceDistributionMap.get(maternalOrangePaternalNonOrangeFemaleAllelePair));
+        assertEquals(0.5, inheritanceDistributionMap.get(orangeMaleAllelePair));
+    }
+    
+    // test homozygous female and male with same copy
+    @Test
+    public void getInheritanceDistributionHomozygousAffectedFemaleAffectedMaleTest() {
+        Map<AllelePair, Double> inheritanceDistributionMap = xLinkedInheritance.getInheritanceDistribution(gene, homozygousOrangeFemaleAllelePair, orangeMaleAllelePair);
+        assertTrue(inheritanceDistributionMap.size() == 2);
+        assertTrue(inheritanceDistributionMap.containsKey(homozygousOrangeFemaleAllelePair));
+        assertTrue(inheritanceDistributionMap.containsKey(orangeMaleAllelePair));
+        assertFalse(inheritanceDistributionMap.containsKey(nonOrangeMaleAllelePair));
+        assertEquals(0.5, inheritanceDistributionMap.get(homozygousOrangeFemaleAllelePair));
+        assertEquals(0.5, inheritanceDistributionMap.get(orangeMaleAllelePair));
     }
 }
