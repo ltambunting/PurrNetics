64c19f9ac92ee0359878538e8f5ea09ff8b831a5
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-25 14:59:50 -0700

    Adj: changed fields in BreedingResult record class

--- src/main/java/com/purrnetics/model/BreedingResult.java
+++ src/main/java/com/purrnetics/model/BreedingResult.java
@@ -3,6 +3,6 @@ package com.purrnetics.model;
 import java.util.Map;
 
 public record BreedingResult() {
-    private static Map<AllelePair, Double> genotypeDistribution;
-    private static Map<String, Double> phenotypeDistribution;
+    private static Map<Gene, Map<AllelePair, Double>> genotypeDistribution;
+    private static Map<Trait, Map<String, Double>> phenotypeDistribution;
 }
