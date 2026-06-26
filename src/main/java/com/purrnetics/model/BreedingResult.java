ad4a1d591718febc276a47ffd0f6754fb9fd15ec
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-25 17:38:43 -0700

    Adj: changed signature of BreedingResult record class

--- src/main/java/com/purrnetics/model/BreedingResult.java
+++ src/main/java/com/purrnetics/model/BreedingResult.java
@@ -2,7 +2,4 @@ package com.purrnetics.model;
 
 import java.util.Map;
 
-public record BreedingResult() {
-    private static Map<Gene, Map<AllelePair, Double>> genotypeDistribution;
-    private static Map<Trait, Map<String, Double>> phenotypeDistribution;
-}
+public record BreedingResult(Map<Gene, Map<AllelePair, Double>> genotypeDistribution, Map<Trait, Map<String, Double>> phenotypeDistribution) {}
