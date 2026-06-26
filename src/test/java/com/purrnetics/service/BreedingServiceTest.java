87a8c6df32281a582eb5bbb9d644008c1478cf10
Louisa Tambunting <louisatambunting@gmail.com>
2026-06-25 23:08:04 -0700

    Adj: removed redundant if statement

--- src/test/java/com/purrnetics/service/BreedingServiceTest.java
+++ src/test/java/com/purrnetics/service/BreedingServiceTest.java
@@ -418,10 +418,6 @@ public class BreedingServiceTest {
         for (Double probability : distributionMap.values()) {
             counter += probability;
         }
-        if (counter == 1.0) {
-            return true;
-        }
-
-        return false;
+        return counter == 1.0;
     }
 }
