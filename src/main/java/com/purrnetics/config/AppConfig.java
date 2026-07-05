0317f25d9fddecb6a656c212d1b0c04cd3fe2fc2
Louisa Tambunting <louisatambunting@gmail.com>
2026-07-04 23:19:54 -0700

    Add: created AppConfig class with appropriate bean annotations for Random, GeneFactory, PresetCatFactory instantiation methods

---
+++ src/main/java/com/purrnetics/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.purrnetics.config;
+
+import java.util.Random;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.purrnetics.factory.GeneFactory;
+import com.purrnetics.factory.PresetCatFactory;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Random random() {
+        return new Random();
+    }
+
+    @Bean
+    public GeneFactory geneFactory() {
+        return new GeneFactory();
+    }
+
+    @Bean
+    public PresetCatFactory presetCatFactory(GeneFactory geneFactory) {
+        return new PresetCatFactory(geneFactory);
+    }
+}
