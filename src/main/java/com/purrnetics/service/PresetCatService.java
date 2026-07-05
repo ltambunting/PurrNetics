dc555bd51f30e130d32d66c5ab451cd703b78cff
Louisa Tambunting <louisatambunting@gmail.com>
2026-07-04 18:52:37 -0700

    Adj: added presetCatMap field and getCat method

--- src/main/java/com/purrnetics/service/PresetCatService.java
+++ src/main/java/com/purrnetics/service/PresetCatService.java
@@ -1,25 +1,39 @@
 package com.purrnetics.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
 
 import com.purrnetics.factory.PresetCatFactory;
 import com.purrnetics.model.Cat;
 
+@Service
 public class PresetCatService {
+    private static final String CAT_ID_1 = "cid1";
+    private static final String CAT_ID_2 = "cid2";
+    private static final String CAT_ID_3 = "cid3";
+    private static final String CAT_ID_4 = "cid4";
+    private static final String CAT_ID_5 = "cid5";
+    private final Map<String, Cat> presetCatMap;
     private PresetCatFactory presetCatFactory;
 
     public PresetCatService(PresetCatFactory presetCatFactory) {
         this.presetCatFactory = presetCatFactory;
+            
+        this.presetCatMap = new HashMap<>();
+        presetCatMap.put(CAT_ID_1, presetCatFactory.createFemaleShortHairAgoutiTortieCat());
+        presetCatMap.put(CAT_ID_2, presetCatFactory.createFemaleShortHairAgoutiOrangeCat());
+        presetCatMap.put(CAT_ID_3, presetCatFactory.createFemaleShortHairAgoutiNonOrangeCat());
+        presetCatMap.put(CAT_ID_4, presetCatFactory.createMaleShortHairAgoutiOrangeCat());
+        presetCatMap.put(CAT_ID_5, presetCatFactory.createMaleShortHairAgoutiNonOrangeCat());
     }
 
-    public List<Cat> getPresetCatList() {
-        List<Cat> presetCatList = new ArrayList<>();
-        presetCatList.add(presetCatFactory.createFemaleShortHairAgoutiTortieCat());
-        presetCatList.add(presetCatFactory.createFemaleShortHairAgoutiOrangeCat());
-        presetCatList.add(presetCatFactory.createFemaleShortHairAgoutiNonOrangeCat());
-        presetCatList.add(presetCatFactory.createMaleShortHairAgoutiOrangeCat());
-        presetCatList.add(presetCatFactory.createMaleShortHairAgoutiNonOrangeCat());
-        return presetCatList;
+    public Map<String, Cat> getPresetCatMap() {
+        return this.presetCatMap;
+    }
+
+    public Cat getCat(String id) {
+        return presetCatMap.get(id);
     }
 }
--- src/test/java/com/purrnetics/service/PresetCatServiceTest.java
+++ src/test/java/com/purrnetics/service/PresetCatServiceTest.java
@@ -1,18 +1,15 @@
 package com.purrnetics.service;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.purrnetics.factory.GeneFactory;
 import com.purrnetics.factory.PresetCatFactory;
 import com.purrnetics.model.Cat;
-import com.purrnetics.model.Sex;
 
 public class PresetCatServiceTest {
+    private static final String CAT_ID_1 = "cid1";
     private GeneFactory geneFactory;
     private PresetCatFactory presetCatFactory;
     private PresetCatService presetCatService;
@@ -25,39 +22,19 @@ public class PresetCatServiceTest {
     }
 
     @Test
-    public void getPresetCatsReturnsAllExpectedCatsTest() {
-        List<Cat> cats = presetCatService.getPresetCatList();
-        assertEquals(5, cats.size());
-        assertTrue(containsCatNamed(cats, "Tortie Queen"));
-        assertTrue(containsCatNamed(cats, "Tiger Queen"));
-        assertTrue(containsCatNamed(cats, "Tabby Queen"));
-        assertTrue(containsCatNamed(cats, "Tiger Tom"));
-        assertTrue(containsCatNamed(cats, "Tabby Tom"));
+    public void getAllPresetCatsReturnsCorrectSizeTest() {
+        assertEquals(5, presetCatService.getPresetCatMap().size());
     }
 
     @Test
-    public void getPresetCatsHasExpectedSexDistributionTest() {
-        List<Cat> cats = presetCatService.getPresetCatList();
-        assertEquals(3, countCatsBySex(cats, Sex.FEMALE));
-        assertEquals(2, countCatsBySex(cats, Sex.MALE));
+    public void getCatByIdReturnsCorrectCatTest() {
+        Cat cat = presetCatService.getPresetCatMap().get("cid1");
+        assertEquals("cid1", cat.getID());
     }
 
-    private boolean containsCatNamed(List<Cat> cats, String name) {
-        for (Cat cat : cats) {
-            if (cat.getName().equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private int countCatsBySex(List<Cat> cats, Sex sex) {
-        int count = 0;
-        for (Cat cat : cats) {
-            if (cat.getSex() == sex) {
-                count++;
-            }
-        }
-        return count;
+    @Test
+    public void getCatTest() {
+        Cat cat = presetCatService.getCat(CAT_ID_1);
+        assertEquals(CAT_ID_1, cat.getID());
     }
 }
