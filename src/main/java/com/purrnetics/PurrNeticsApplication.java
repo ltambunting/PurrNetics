6988189f23ad1132b8228c0fe0b0cd5844d7be74
Louisa Tambunting <louisatambunting@gmail.com>
2026-07-04 23:21:47 -0700

    Add: created BreedingController class and updated PurrNeticsApplication class

---
+++ src/main/java/com/purrnetics/PurrNeticsApplication.java
@@ -0,0 +1,11 @@
+package com.purrnetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurrNeticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PurrNeticsApplication.class, args);
+        }
+}
---
+++ src/main/java/com/purrnetics/controller/BreedingController.java
@@ -0,0 +1,43 @@
+package com.purrnetics.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.purrnetics.dto.BreedingRequestDto;
+import com.purrnetics.dto.BreedingResponseDto;
+import com.purrnetics.mapper.BreedingMapper;
+import com.purrnetics.model.BreedingResult;
+import com.purrnetics.model.Cat;
+import com.purrnetics.model.ParentPair;
+import com.purrnetics.service.BreedingService;
+import com.purrnetics.service.PresetCatService;
+
+@RestController
+@RequestMapping("/breed")
+public class BreedingController {
+    private final BreedingService breedingService;
+    private final PresetCatService presetCatService;
+    private final BreedingMapper breedingMapper = new BreedingMapper();
+
+    public BreedingController(BreedingService breedingService, PresetCatService presetCatService) {
+        this.breedingService = breedingService;
+        this.presetCatService = presetCatService;
+    }
+
+    @PostMapping("/result")
+    public BreedingResponseDto breed(@RequestBody BreedingRequestDto request) {
+        Cat mother = presetCatService.getCat(request.motherId());
+        Cat father = presetCatService.getCat(request.fatherId());
+        if (mother == null || father == null) {
+            throw new IllegalArgumentException("Invalid cat ID");
+        }
+        ParentPair parentPair = new ParentPair(mother, father);
+        BreedingResult breedingResult = breedingService.getBreedingResult(parentPair);
+        return breedingMapper.toDto(parentPair, breedingResult);
+    }
+}
+
+
+
